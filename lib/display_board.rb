078d8e1aaa9828ac018c42906b4f005b9d93537e
Seth Mueller <sethmueller1@gmail.com>
2020-05-30 06:05:38 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,9 +2,10 @@
 # out the current state.
 
 def display_board(board)
-puts "   |   |   "
-puts "-----------"
-puts "   |   |   "
-puts "-----------"
-puts "   |   |   "
+  board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
+  puts "   |   |   "
+  puts "-----------"
+  puts "   | X |   "
+  puts "-----------"
+  puts "   |   |   "
 end
