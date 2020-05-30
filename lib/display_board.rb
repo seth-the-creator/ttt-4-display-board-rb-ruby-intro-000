d3742c64b7592c0efc7fa26509dbf9152e70d367
Seth Mueller <sethmueller1@gmail.com>
2020-05-30 05:36:58 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,10 +1,12 @@
 # Define display_board that accepts a board and prints
 # out the current state.
-def display_board
+
+def display_board(board)
 puts "   |   |   "
 puts "-----------"
 puts "   |   |   "
 puts "-----------"
 puts "   |   |   "
 end
+
 board = ["O", "X", " ", " ", "X", " ", "X", "O", " "]
